c482777e09ceb9201608cef5c2262c87962a3a39
sarahmatishoi <sarahmatishoi@gmail.com>
2021-02-21 10:35:09 +0300

    first commit

---
+++ src/hello.kt
@@ -0,0 +1,32 @@
+fun main() {
+    var name:String="ruth leshan"
+    println(name)
+    var age:Int=24
+    println(age)
+    var phoneNumber:String="0759221716"
+    println(phoneNumber)
+    var kenyan:Boolean=true
+    println(kenyan)
+    var weight:Float=60.2f
+    println(weight)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
