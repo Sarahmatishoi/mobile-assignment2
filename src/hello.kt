1ca8b01ae664c036abe4370bbd6baf425cc63a8a
sarahmatishoi <sarahmatishoi@gmail.com>
2021-02-27 13:42:20 +0300

    first commit

--- src/hello.kt
+++ src/hello.kt
@@ -1,17 +1,24 @@
 fun main() {
-    var name:String="ruth leshan"
-    println(name)
-    var age:Int=24
-    println(age)
-    var phoneNumber:String="0759221716"
-    println(phoneNumber)
-    var kenyan:Boolean=true
-    println(kenyan)
-    var weight:Float=60.2f
-    println(weight)
-
+add()
+sarah()
+var result =add(10,20)
+    print(result)
 }
 
+fun add() {
+    var mango = 20
+    var orange = 10
+    var total = mango + orange
+    print(total)
+}
+fun sarah() {
+    println("sarah")
+}
+fun add(a:Int,b:Int): Int {
+    var total=a+b
+    return total
+
+}
 
 
 
