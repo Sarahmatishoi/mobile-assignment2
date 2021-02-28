95dbf0c5333297fc96fc5e02133d48f611436022
sarahmatishoi <sarahmatishoi@gmail.com>
2021-02-28 12:14:51 +0300

    first commit

--- src/hello.kt
+++ src/hello.kt
@@ -1,26 +1,28 @@
 fun main() {
-add()
-sarah()
-var result =add(10,20)
-    print(result)
-}
+ sarah ()
+  Division ()
+    var result=add(20,10,5,15)
+    println(result)
+interest()
 
-fun add() {
-    var mango = 20
-    var orange = 10
-    var total = mango + orange
-    print(total)
+
 }
 fun sarah() {
-    println("sarah")
+    println("Hello sarah")
 }
-fun add(a:Int,b:Int): Int {
-    var total=a+b
+fun Division() {
+    var apple=72
+    var student=9
+    var total=apple/student
+    println(total)
+}
+fun add(a:Int,b:Int,c:Int,d:Int):Int {
+    var total=a+b+c+d
     return total
-
 }
-
-
+fun interest() {
+    println("playingNetball")
+}
 
 
 
